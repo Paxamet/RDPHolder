79baf820ac66a1bb367a56e1f966e0415b6528c5
Paxamet <paxamet@gmail.com>
2023-12-01 19:08:41 +0300

    Плучение списка процессов; Проверка наличия текущего процесса RDP в списке запущенных; Завершение клиентского модуля при отсутствии процесса RDP;

--- src/Client/ClientRoot.java
+++ src/Client/ClientRoot.java
@@ -1,11 +1,18 @@
 package Client;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class ClientRoot {
 
 	public static void main(String[] args) {
-		OnRDPStart();
+		System.out.println("Client::BEGIN");
+		long processID = OnRDPStart();
+		
+		CheckRDPProcess(processID);
+		
+		System.out.println("Client::END");
 	}
 	
 	public static long OnRDPStart() {
@@ -19,7 +26,43 @@ public class ClientRoot {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+					
 		return result;
+	}
+	
+	public static void CheckRDPProcess(long processID) {
+		boolean isRDPProcessFinded = true;
+		while(isRDPProcessFinded) {
+			isRDPProcessFinded = false;
+			Process process = null;
+			try {
+				process = Runtime.getRuntime().exec("tasklist.exe /fo csv /nh");
+						
+				BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()));
+				for(String processStr = input.readLine(); processStr != null; processStr = input.readLine()) {
+					String[] processArr = processStr.split(",");
+					if(processArr.length < 2) {
+						System.out.println("ERROR processStr: " + processStr);
+						continue;
+					}
+					
+					if(Integer.valueOf(processArr[1].replace("\"", "")) == processID) {
+						isRDPProcessFinded = true;
+						System.out.println("Process " + processID + " is running");
+						Thread.sleep(10 * 1000);
+						break;
+					}
+				}
+				input.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		System.out.println("Process " + processID + " is finished");
 	}
 
 }
